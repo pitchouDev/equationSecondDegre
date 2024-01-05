1dd2b3c2ca8ca2d8daa84089b93dae3d7597c332
pitchouDev <ibra1louz@gmail.com>
2024-01-05 22:21:21 +0100

    V1.0, fonctionel, réponse inprécises, media querie à corriger pour plus de flexibilité

---
+++ script.js
@@ -0,0 +1,49 @@
+const bouton = document.getElementById("bouton");
+
+const reponseDelta = document.getElementById("delta");
+const reponseXUn = document.getElementById("xUn");
+const reponseXDeux = document.getElementById("xDeux")
+
+document.addEventListener("click", (event) => {
+    if (event.target === bouton) {
+        
+        let a = parseFloat(document.getElementById("a").value)
+        let b = parseFloat(document.getElementById("b").value)
+        let c = parseFloat(document.getElementById("c").value)
+        if (!a || a === 0) {
+            alert('"a" ne peut pas être égal à 0')
+            return;
+        }
+        if (!b) {
+            b = 0
+        }
+        if (!c) {
+            c = 0
+        }
+
+        let xUn;
+        let xDeux;
+
+        let delta = Math.pow(b, 2) - 4 * a * c;
+        reponseDelta.innerText = `Δ = ${delta}`
+
+        if (delta < 0) {
+            reponseXUn.innerText = "Aucune solution dans ℝ"
+            reponseXDeux.innerText = ""
+        }
+        else if (delta === 0) {
+            xUn = (- b - Math.sqrt(delta)) / (2 * a)
+            reponseXUn.innerText = `x = ${xUn}`
+        } else {
+            xUn = (- b - Math.sqrt(delta)) / (2 * a)
+            xDeux = (- b + Math.sqrt(delta)) / (2 * a)
+            reponseXUn.innerText = `x = ${xUn}`
+            reponseXDeux.innerText = `x = ${xDeux}`
+
+        }
+
+    }
+})
+
+
+
